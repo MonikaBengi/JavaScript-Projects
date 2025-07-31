0823b892184f1ba58eaa08d65107a973b97ab554
MonikaBengi <112941981+MonikaBengi@users.noreply.github.com>
2025-07-31 13:03:08 +0100

    "Finished Project1_expressions_alert"

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,17 @@
+// This variable stores the beginning of the sentence
+var Sent1 = "This is the beginning of the string";
+
+// This variable stores the end of the sentence
+var Sent2 = " and this is the end of the string";
+
+// This line writes the concatenated string to the page
+document.write(Sent1 + Sent2);
+
+// This line stores the result of a mathematical expression
+var result = 5 + 7;
+
+// This displays the result on the page
+document.write("<br>" + result);
+
+// This creates an alert box
+window.alert("Hello from my JavaScript project!");
