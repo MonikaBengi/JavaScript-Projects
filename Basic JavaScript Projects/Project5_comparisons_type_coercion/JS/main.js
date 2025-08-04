6623197491d33c2f2405ec979aba94be5de01aa9
MonikaBengi <112941981+MonikaBengi@users.noreply.github.com>
2025-08-04 12:39:05 +0100

    Completed Project5_comparisons_type_coercion assignment

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,33 @@
+// Display the data type of a variable
+var name = "Monika";
+document.write(typeof name); // Output: string
+
+// Type coercion: string + number
+document.write("10" + 5); // Output: 105
+
+// Double equals (==) checks value
+document.write(10 == "10"); // true
+
+// Triple equals (===) checks value and type
+document.write(10 === "10"); // false
+
+// Greater than (>) and less than (<)
+document.write(8 > 3); // true
+document.write(3 < 1); // false
+
+// AND operator
+document.write(5 > 3 && 10 > 2); // true
+
+// OR operator
+document.write(5 > 10 || 10 > 2); // true
+
+// NOT operator
+document.write(!(5 > 10)); // true
+
+// This variable stores a string
+var car = "Toyota";
+// Megnézzük a típusát
+document.write(typeof car); // string
+
+// String + Number => type coercion
+document.write("20" + 10); // 2010
