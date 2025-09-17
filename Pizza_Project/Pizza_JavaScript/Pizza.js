423f8aa69b7912859ba38733c696422c90c50c8b
MonikaBengi <112941981+MonikaBengi@users.noreply.github.com>
2025-09-17 13:16:19 +0100

    Completed Pizza Menu assignment with Italian theme

---
+++ Pizza_Project/Pizza_JavaScript/Pizza.js
@@ -0,0 +1,62 @@
+function getReceipt() {
+    // Initialize receipt text
+    var text1 = "<h3>You Ordered:</h3>";
+    var runningTotal = 0;
+    var sizeTotal = 0;
+    var sizeArray = document.getElementsByClassName("size");
+
+    // Check which size is selected
+    for (var i = 0; i < sizeArray.length; i++) {
+        if (sizeArray[i].checked) {
+            var selectedSize = sizeArray[i].value;
+            text1 = text1 + selectedSize + "<br>";
+        }
+    }
+
+    // Prices for sizes
+    if (selectedSize === "Personal Pizza") {
+        sizeTotal = 6;
+    } else if (selectedSize === "Small Pizza") {
+        sizeTotal = 8;
+    } else if (selectedSize === "Medium Pizza") {
+        sizeTotal = 10;
+    } else if (selectedSize === "Large Pizza") {
+        sizeTotal = 14;
+    } else if (selectedSize === "Extra Large Pizza") {
+        sizeTotal = 16;
+    }
+
+    runningTotal = sizeTotal;
+
+    // Pass variables to toppings function
+    getTopping(runningTotal, text1);
+}
+
+function getTopping(runningTotal, text1) {
+    var toppingTotal = 0;
+    var selectedTopping = [];
+    var toppingArray = document.getElementsByClassName("toppings");
+
+    // Loop through toppings and add selected ones
+    for (var j = 0; j < toppingArray.length; j++) {
+        if (toppingArray[j].checked) {
+            selectedTopping.push(toppingArray[j].value);
+            text1 = text1 + toppingArray[j].value + "<br>";
+        }
+    }
+
+    // First topping is free
+    var toppingCount = selectedTopping.length;
+    if (toppingCount > 1) {
+        toppingTotal = (toppingCount - 1);
+    } else {
+        toppingTotal = 0;
+    }
+
+    runningTotal = (runningTotal + toppingTotal);
+
+    // Show receipt and price
+    document.getElementById("showText").innerHTML = text1;
+    document.getElementById("totalPrice").innerHTML =
+        "<h3>Total: <strong>$" + runningTotal + ".00</strong></h3>";
+}
